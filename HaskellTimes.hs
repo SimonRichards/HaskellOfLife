21044cd499e105274627ababe777e1314644a397
George <georgina.carvell@gmail.com>
2015-09-05 10:16:08 -0700

    Generate universe

--- HaskellTimes.hs
+++ HaskellTimes.hs
@@ -1,3 +1,6 @@
+import System.Random
+import Data.List.Split
+
 checkingThings :: Int -> Int -> [[Int]] -> Int
 checkingThings i j world =
     
@@ -10,8 +13,13 @@ checkingThings i j world =
             then 1 
             else 0)
 
+
 sumNeighbours:: Int -> Int -> [[Int]] -> Int
 sumNeighbours i j world = sum [get (i-1) (j-1) world, get (i-1) j world, get (i-1) (j+1) world, get i (j-1) world, get i (j+1) world, get (i+1) (j-1) world, get (i+1) j world, get (i+1) (j+1) world]
+
+playTheGame :: [[Int]] -> [[Int]]
+playTheGame (x:xs) = x:xs
+    -- in nextGen = (checkingThings row col xs):playTheGame xs 
 
 get :: Int -> Int -> [[Int]] -> Int
 get i j world = 
@@ -26,3 +34,9 @@ get i j world =
                 then 0
                 else j
     in world!!i'!!j'
+
+main = do
+  g <- newStdGen
+  let universe = splitPlaces (replicate 10 10) (take 100 $ (randomRs (0, 1) g :: [Int]))
+  print universe
+  print $ playTheGame universe
